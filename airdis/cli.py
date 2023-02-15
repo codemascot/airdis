d8e177df7a5d33a9bf6639f650da330fa648f667
Khan Mohammad R <codemascot@hotmail.com>
2023-02-15 12:20:49 +0100

    Refactorred the CLI func a bit

--- airdis/cli.py
+++ airdis/cli.py
@@ -5,6 +5,7 @@ import os
 
 from typing import Tuple
 
+
 def process_args(parser: argparse.ArgumentParser) -> Tuple[int, str]:
     """
     This function is used to process the command-line arguments passed to the script.
@@ -14,20 +15,20 @@ def process_args(parser: argparse.ArgumentParser) -> Tuple[int, str]:
     - Tuple[int, str] -- Tuple of two values: number of random places to pick and the path to the places.csv.
     """
     parser.add_argument(
-        '-n',
-        '--number',
+        "-n",
+        "--number",
         help="Number of random places to pick form the places list",
-        type= int,
-        default= 0
+        type=int,
+        default=0,
     )
-    
+
     parser.add_argument(
-        '-p',
-        '--path',
+        "-p",
+        "--path",
         help="Path to the places.csv",
-        type= str,
-        default= os.path.join(os.path.dirname(__file__), '../data/places.csv')
+        type=str,
+        default=os.path.join(os.path.dirname(__file__), "../data/places.csv"),
     )
-    
+
     args = parser.parse_args()
     return args.number, args.path
